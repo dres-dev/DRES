9d1756800b2b9d1bd3822a2a3c36fe0a5393a70a
Luca Rossetto <rossetto@ifi.uzh.ch>
2020-03-21 15:59:38 +0100

    Got OpenApi documentation generation to work with rest handler abstraction. Explicit definition of handler path in Annotation is required.

--- backend/src/main/kotlin/dres/api/rest/handler/GetVersionHandler.kt
+++ backend/src/main/kotlin/dres/api/rest/handler/GetVersionHandler.kt
@@ -3,8 +3,11 @@ package dres.api.rest.handler
 import dres.api.rest.RestApiRole
 import io.javalin.core.security.Role
 import io.javalin.http.Context
+import io.javalin.plugin.openapi.annotations.OpenApi
 
 class GetVersionHandler : GetRestHandler, AccessManagedRestHandler {
+
+    @OpenApi(summary = "returns the API version", path = "/api/version")
     override fun get(ctx: Context) {
         ctx.result("0.1")
     }

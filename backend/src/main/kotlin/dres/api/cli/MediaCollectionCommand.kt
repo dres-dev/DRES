c31babce3358baf114f848fb4efde7ef46f2c41f
Luca Rossetto <lucaro@lucaro.ch>
2020-03-22 20:57:22 +0100

    Added commands to manually add media items to a collection and list them

--- backend/src/main/kotlin/dres/api/cli/MediaCollectionCommand.kt
+++ backend/src/main/kotlin/dres/api/cli/MediaCollectionCommand.kt
@@ -3,20 +3,22 @@ package dres.api.cli
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.NoOpCliktCommand
 import com.github.ajalt.clikt.core.subcommands
-import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
-import com.github.ajalt.clikt.parameters.options.validate
+import com.github.ajalt.clikt.parameters.options.*
+import com.github.ajalt.clikt.parameters.types.float
+import com.github.ajalt.clikt.parameters.types.long
 import dres.data.dbo.DAO
 import dres.data.model.basics.MediaCollection
 import dres.data.model.basics.MediaItem
 import dres.data.model.basics.MediaItemSegment
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.time.Duration
 
 class MediaCollectionCommand(val collections: DAO<MediaCollection>, val items: DAO<MediaItem>, val segments: DAO<MediaItemSegment>) :
         NoOpCliktCommand(name = "collection") {
 
     init {
-        this.subcommands(CreateCollectionCommand(), ListCollectionsCommand())
+        this.subcommands(CreateCollectionCommand(), ListCollectionsCommand(), ShowCollectionCommand(), AddMediaItemCommand())
     }
 
     inner class CreateCollectionCommand : CliktCommand(name = "create") {
@@ -43,6 +45,84 @@ class MediaCollectionCommand(val collections: DAO<MediaCollection>, val items: D
                 println(it)
             }
         }
+    }
+
+    inner class ShowCollectionCommand : CliktCommand(name = "show") {
+
+        private val collectionId: Long by option("-c", "--collection")
+                .convert { this@MediaCollectionCommand.collections.find { c -> c.name == it }?.id ?: -1 }
+                .required()
+                .validate { require(it > -1) {"Collection not found"} }
+
+        override fun run() {
+            this@MediaCollectionCommand.items.filter{ it.collection == collectionId}.forEach {
+                println(it)
+            }
+        }
+
+    }
+
+    inner class AddMediaItemCommand : NoOpCliktCommand(name = "add") {
+
+        init {
+            this.subcommands(AddImageCommand(), AddVideoCommans())
+        }
+
+
+        inner class AddImageCommand : CliktCommand(name = "image") {
+
+            private val collectionId: Long by option("-c", "--collection")
+                    .convert { this@MediaCollectionCommand.collections.find { c -> c.name == it }?.id ?: -1 }
+                    .required()
+                    .validate { require(it > -1) {"Collection not found"} }
+
+            private val name: String by option("-n", "--name").required()
+            private val path: Path by option("-p", "--path")
+                    .convert { Paths.get(it) }
+                    .required()
+
+            override fun run() {
+                val existing = this@MediaCollectionCommand.items.filterIsInstance<MediaItem.ImageItem>().find { it.collection == collectionId && it.name == name }
+                if (existing != null) {
+                    println("item with name '$name' already exists in collection:")
+                    println(existing)
+                    return
+                }
+                this@MediaCollectionCommand.items.append(MediaItem.ImageItem(name = name, location = path, collection = collectionId, id = -1))
+                println("item added")
+            }
+
+        }
+
+        inner class AddVideoCommans : CliktCommand(name = "video"){
+
+            private val collectionId: Long by option("-c", "--collection")
+                    .convert { this@MediaCollectionCommand.collections.find { c -> c.name == it }?.id ?: -1 }
+                    .required()
+                    .validate { require(it > -1) {"Collection not found"} }
+
+            private val name: String by option("-n", "--name").required()
+            private val path: Path by option("-p", "--path")
+                    .convert { Paths.get(it) }
+                    .required()
+
+            private val duration: Long by option("-d", "--duration", help = "video duration in seconds").long().required()
+            private val fps: Float by option("-f", "--fps").float().required()
+
+            override fun run() {
+                val existing = this@MediaCollectionCommand.items.filterIsInstance<MediaItem.VideoItem>().find { it.collection == collectionId && it.name == name }
+                if (existing != null) {
+                    println("item with name '$name' already exists in collection:")
+                    println(existing)
+                    return
+                }
+                this@MediaCollectionCommand.items.append(MediaItem.VideoItem(name = name, location = path, collection = collectionId, duration = Duration.ofSeconds(duration), fps = fps, id = -1))
+                println("item added")
+            }
+
+        }
+
+
 
     }
 }
--- backend/src/main/kotlin/dres/data/model/basics/MediaItem.kt
+++ backend/src/main/kotlin/dres/data/model/basics/MediaItem.kt
@@ -17,7 +17,10 @@ sealed class MediaItem : Entity {
         const val IMAGE_MEDIA_ITEM = 1
     }
 
-    data class ImageItem(override var id: Long, val name: String, val location: Path, val collection: Long): MediaItem()
+    abstract val collection: Long
+    abstract val name: String
 
-    data class VideoItem(override var id: Long, val name: String, val location: Path, val collection: Long, val duration: Duration, val fps: Float): MediaItem()
+    data class ImageItem(override var id: Long, override val name: String, val location: Path, override val collection: Long): MediaItem()
+
+    data class VideoItem(override var id: Long, override val name: String, val location: Path, override val collection: Long, val duration: Duration, val fps: Float): MediaItem()
 }

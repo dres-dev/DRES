a27cbe5e5cf9040ada04e2f0a35ecc050dff9a85
Luca Rossetto <rossetto@ifi.uzh.ch>
2020-03-22 10:00:35 +0100

    Started to replace primitives with semantically meaningful types
    inspired by https://www.youtube.com/watch?v=t3DBzaeid74

--- backend/src/main/kotlin/dres/Playground.kt
+++ backend/src/main/kotlin/dres/Playground.kt
@@ -2,8 +2,10 @@ package dres
 
 import dres.data.dbo.DAO
 import dres.data.model.Config
+import dres.data.model.admin.PlainPassword
 import dres.data.model.admin.Role
 import dres.data.model.admin.User
+import dres.data.model.admin.UserName
 import dres.data.serializers.UserSerializer
 import org.mindrot.jbcrypt.BCrypt
 import java.nio.file.Paths
@@ -17,7 +19,7 @@ object Playground {
 
         val dao = DAO(Paths.get(config.dataPath + "/users.db"), UserSerializer)
 
-        val user =  User(username = "testuser", password = BCrypt.hashpw("password", BCrypt.gensalt()), role = Role.ADMIN)
+        val user =  User(username = UserName("testuser"), password = PlainPassword("password").hash(), role = Role.ADMIN)
 
         dao.append(user)
 

aff38b9047309afa40333962a63ffa8047337bc6
Ralph Gasser <rg@pontius.ch>
2020-03-22 14:14:34 +0100

    Added command to update current user.

--- backend/src/main/kotlin/dres/api/cli/UserCommand.kt
+++ backend/src/main/kotlin/dres/api/cli/UserCommand.kt
@@ -3,6 +3,7 @@ package dres.api.cli
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.NoOpCliktCommand
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.options.validate
@@ -26,19 +27,27 @@ class UserCommand (val users: DAO<User>) : NoOpCliktCommand(name = "users") {
     }
 
     init {
-        this.subcommands(CreateUserCommand(), DeleteUserCommand(), ListUsers(), ListRoles())
+        this.subcommands(CreateUserCommand(), UpdateUserCommand(), DeleteUserCommand(), ListUsers(), ListRoles())
     }
 
     /**
      * [CliktCommand] to create a new [User].
      */
     inner class CreateUserCommand : CliktCommand(name = "create") {
-        val username: String by option("-u", "--username").required().validate { require(it.length >= MIN_LENGTH_USERNAME) { "Username for DRES user must consist of at least $MIN_LENGTH_USERNAME characters." } }
-        val password: String by option("-p", "--password").required().validate { require(it.length >= MIN_LENGTH_PASSWORD) { "Password for DRES user must consist of at least $MIN_LENGTH_PASSWORD characters." } }
-        val role: Role by option("-r", "--role").enum<Role>().required()
+        private val username: UserName by option("-u", "--username")
+                .convert { UserName(it) }
+                .required()
+                .validate { require(it.name.length >= MIN_LENGTH_USERNAME) { "Username for DRES user must consist of at least $MIN_LENGTH_USERNAME characters." } }
+
+        private val password: PlainPassword by option("-p", "--password")
+                .convert { PlainPassword(it) }
+                .required()
+                .validate { require(it.pass.length >= MIN_LENGTH_PASSWORD) { "Password for DRES user must consist of at least $MIN_LENGTH_PASSWORD characters." } }
+
+        private val role: Role by option("-r", "--role").enum<Role>().required()
 
         override fun run() {
-            val newUser = User(username = UserName(this.username), password = PlainPassword(this.password).hash(), role = role)
+            val newUser = User(username = this.username, password = this.password.hash(), role = role)
             for (existingUser in this@UserCommand.users) {
                 if (existingUser.username == newUser.username) {
                     println("Could not create user '${newUser}' because a user with that name already exists.")
@@ -51,23 +60,56 @@ class UserCommand (val users: DAO<User>) : NoOpCliktCommand(name = "users") {
     }
 
     /**
+     * [CliktCommand] to update an existing [User].
+     */
+    inner class UpdateUserCommand : CliktCommand(name = "update") {
+        private val id: Long? by option("-i", "--id").long()
+        private val username: UserName? by option("-u", "--username")
+                .convert { UserName(it) }
+                .validate { require(it.name.length >= MIN_LENGTH_USERNAME) { "Username for DRES user must consist of at least $MIN_LENGTH_USERNAME characters." } }
+
+        private val password: PlainPassword? by option("-p", "--password")
+                .convert { PlainPassword(it) }
+                .validate { require(it.pass.length >= MIN_LENGTH_PASSWORD) { "Password for DRES user must consist of at least $MIN_LENGTH_PASSWORD characters." } }
+
+        private val role: Role? by option("-r", "--role").enum<Role>()
+
+        override fun run() {
+            val updateId = when {
+                this.id != null -> this.id!!
+                this.username != null -> this@UserCommand.users.find { it.username == this.username!! }?.id
+                else -> null
+            }
+
+            if (updateId != null) {
+                val currentUser = this@UserCommand.users[updateId]
+                if (currentUser != null) {
+                    val updatedUser = currentUser.copy(id = currentUser.id, username = currentUser.username, password = this.password?.hash() ?: currentUser.password, role = this.role ?: currentUser.role )
+                    this@UserCommand.users.update(updatedUser)
+                    println("User $updateId updated successfully (old: $currentUser, new: $updatedUser)!")
+                } else {
+                    println("User $updateId could not be updated because it doesn't exist!")
+                }
+            } else {
+                println("You must specify a valid username or user ID in order to delete a user!")
+            }
+        }
+    }
+
+    /**
      * [CliktCommand] to delete a [User].
      */
     inner class DeleteUserCommand : CliktCommand(name = "delete") {
-        val id: Long? by option("-i", "--id").long()
-        val username: String? by option("-u", "--username").validate { require(it.length >= MIN_LENGTH_USERNAME) { "Username for DRES user must consist of at least $MIN_LENGTH_USERNAME characters." } }
+        private val id: Long? by option("-i", "--id").long()
+        private val username: UserName? by option("-u", "--username")
+                .convert { UserName(it) }
+                .validate { require(it.name.length >= MIN_LENGTH_USERNAME) { "Username for DRES user must consist of at least $MIN_LENGTH_USERNAME characters." } }
 
         override fun run() {
             val id = when {
-                this.id != null -> {
-                    this.id!!
-                }
-                this.username != null -> {
-                    this@UserCommand.users.find { it.username == UserName(this.username!!) }?.id
-                }
-                else -> {
-                    null
-                }
+                this.id != null -> this.id!!
+                this.username != null -> this@UserCommand.users.find { it.username == this.username!! }?.id
+                else -> null
             }
 
             if (id != null) {

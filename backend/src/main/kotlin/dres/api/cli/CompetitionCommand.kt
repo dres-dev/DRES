4916f9f6580ea441fae74b3ee79ecdf0b9926d33
Luca Rossetto <lucaro@lucaro.ch>
2020-03-25 20:14:44 +0100

    Started on CLI commands to manage competitions

--- backend/src/main/kotlin/dres/api/cli/CompetitionCommand.kt
+++ backend/src/main/kotlin/dres/api/cli/CompetitionCommand.kt
@@ -1,14 +1,57 @@
 package dres.api.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.core.NoOpCliktCommand
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.convert
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.options.validate
+import dres.data.dbo.DAO
+import dres.data.model.competition.Competition
 
-class CompetitionCommand : CliktCommand(name = "competition") {
+class CompetitionCommand(val competitions: DAO<Competition>) : NoOpCliktCommand(name = "competition") {
 
-    val action by argument()
-
-    override fun run() {
-        println("competition action selected: $action")
-        //TODO("Not yet implemented")
+    init {
+        this.subcommands(ListCompetitionCommand(), ShowCompetitionCommand())
     }
+
+    abstract inner class AbstractCompetitionCommand(private val name: String) : CliktCommand(name = name) {
+
+        protected val competitionId: Long by option("-c", "--competition")
+                .convert { this@CompetitionCommand.competitions.find { c -> c.name == it }?.id ?: -1 }
+                .required()
+                .validate { require(it > -1) {"Competition not found"} }
+
+    }
+
+    inner class ListCompetitionCommand : CliktCommand(name = "list") {
+        override fun run() {
+            println("Competitions:")
+            this@CompetitionCommand.competitions.forEach {
+                println("${it.name} (${it.teams.size} Teams, ${it.tasks.size} Tasks): ${it.description}")
+            }
+        }
+    }
+
+    inner class ShowCompetitionCommand : AbstractCompetitionCommand(name = "show") {
+
+        override fun run() {
+            val competition = this@CompetitionCommand.competitions[competitionId]!!
+
+            println("${competition.name}: ${competition.description}")
+            println("Teams:")
+
+            competition.teams.forEach (::println)
+
+            println()
+            println("Tasks:")
+
+            competition.tasks.forEach(::println)
+
+            println()
+        }
+
+    }
+
 }

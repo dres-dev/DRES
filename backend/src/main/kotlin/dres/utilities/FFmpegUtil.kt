4c750f6e7b0f3cb42b6ddf6f89f2b745997b7fa2
Luca Rossetto <lucaro@lucaro.ch>
2020-03-28 12:09:21 +0100

    Added missing class

---
+++ backend/src/main/kotlin/dres/utilities/FFmpegUtil.kt
@@ -0,0 +1,22 @@
+package dres.utilities
+
+import com.github.kokorin.jaffree.ffmpeg.FFmpeg
+import com.github.kokorin.jaffree.ffmpeg.UrlInput
+import com.github.kokorin.jaffree.ffmpeg.UrlOutput
+import java.nio.file.Path
+
+object FFmpegUtil {
+
+    private val ffmpegBin = Path.of("ext/ffmpeg") //TODO make configurable
+
+    fun extractFrame(video: Path, timecode: String, outputImage: Path) {
+        FFmpeg.atPath(ffmpegBin)
+            .addInput(UrlInput.fromPath(video))
+            .addOutput(UrlOutput.toPath(outputImage))
+            .setOverwriteOutput(true)
+            .addArguments("-ss", timecode)
+            .addArguments("-vframes", "1")
+            .execute()
+    }
+
+}

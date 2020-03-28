20b917f6c1df7ede62119a28e997ffad9d84de67
Luca Rossetto <lucaro@lucaro.ch>
2020-03-28 11:02:34 +0100

    Refactoring in CompetitionHandler to make it more internally consistent and more REST like

--- backend/src/main/kotlin/dres/api/rest/handler/CompetitionHandler.kt
+++ backend/src/main/kotlin/dres/api/rest/handler/CompetitionHandler.kt
@@ -14,13 +14,25 @@ import io.javalin.plugin.openapi.annotations.OpenApiResponse
 abstract class CompetitionHandler(protected val competitions: DAO<Competition>) : RestHandler, AccessManagedRestHandler {
 
     override val permittedRoles: Set<Role> = setOf(RestApiRole.ADMIN)
+
+    private fun competitionId(ctx: Context): Long =
+            ctx.pathParamMap().getOrElse("competitionId"){
+                throw ErrorStatus("Parameter 'competitionId' is missing!'")
+            }.toLong()
+
+    private fun competitionById(id: Long): Competition =
+            competitions[id] ?: throw ErrorStatus("Competition with ID $id not found.'")
+
+    protected fun competitionFromContext(ctx: Context): Competition = competitionById(competitionId(ctx))
+
+
 }
 
 class ListCompetitionHandler(competitions: DAO<Competition>) : CompetitionHandler(competitions), GetRestHandler {
 
     @OpenApi(
             summary = "Lists an overview of all available competitions with basic information about their content.",
-            path = "/api/competition/list",
+            path = "/api/competition",
             tags = ["Competition"],
             responses = [
                 OpenApiResponse("200", [OpenApiContent(Array<CompetitionOverview>::class)]),
@@ -33,14 +45,14 @@ class ListCompetitionHandler(competitions: DAO<Competition>) : CompetitionHandle
         )
     }
 
-    override val route: String = "competition/list"
+    override val route: String = "competition"
 }
 
 class GetCompetitionHandler(competitions: DAO<Competition>) : CompetitionHandler(competitions), GetRestHandler {
 
     @OpenApi(
             summary = "Loads the detailed definition of a specific competition.",
-            path = "/api/competition/get/:competitionId",
+            path = "/api/competition/:competitionId",
             pathParams = [OpenApiParam("competitionId", Long::class, "Competition ID")],
             tags = ["Competition"],
             responses = [
@@ -52,21 +64,17 @@ class GetCompetitionHandler(competitions: DAO<Competition>) : CompetitionHandler
     )
     override fun get(ctx: Context) {
 
-        val competitionId = ctx.pathParamMap().getOrElse("competitionId") {
-            ctx.status(400).json(ErrorStatus("Parameter 'competitionId' is missing!'"))
-            return
-        }.toLong()
-
-        val competition = this.competitions[competitionId]
-        if (competition == null){
-            ctx.status(404).json(ErrorStatus("Competition with ID $competitionId not found.'"))
+        val competition  = try{
+            competitionFromContext(ctx)
+        } catch (e: ErrorStatus){
+            ctx.json(e)
             return
         }
 
         ctx.json(competition)
     }
 
-    override val route: String = "competition/get/:competition"
+    override val route: String = "competition/:competitionId"
 }
 
 data class CompetitionOverview(val id: Long, val name: String, val description: String, val taskCount: Int, val teamCount: Int) {

84569510c652268f9323deb45183a9fb509581be
Ralph Gasser <rg@pontius.ch>
2020-03-29 12:11:42 +0200

    Adjusted team data model.

--- backend/src/main/kotlin/dres/data/model/competition/Competition.kt
+++ backend/src/main/kotlin/dres/data/model/competition/Competition.kt
@@ -6,6 +6,25 @@ import java.lang.Long.max
 
 data class Competition(override var id: Long, val name: String, val description: String?, val tasks: MutableList<Task>, val teams: MutableList<Team>) : Entity {
 
+
+    fun validate() {
+        for (task in this.tasks) {
+            if (tasks.map { it.name }.contains(task.name)) {
+                throw IllegalArgumentException("Duplicate task with name '${task.name}'!")
+            }
+        }
+
+        for (team in this.teams) {
+            if (teams.map { it.number }.contains(team.number)) {
+                throw IllegalArgumentException("Duplicate team with number '${team.number}'!")
+            }
+            if (teams.map { it.name }.contains(team.name)) {
+                throw IllegalArgumentException("Duplicate team with name '${team.name}'!")
+            }
+        }
+    }
+
+
     fun addTask(task: Task) {
         task.id = max(0, (tasks.maxBy { it.id }?.id ?: 0) + 1)
 
@@ -17,8 +36,6 @@ data class Competition(override var id: Long, val name: String, val description:
     }
 
     fun addTeam(team: Team){
-        team.id = max(0, (teams.maxBy { it.id }?.id ?: 0) + 1)
-
         if (teams.map { it.name }.contains(team.name)) {
             throw IllegalArgumentException("Team with name ${team.name} already exists")
         }
@@ -26,7 +43,6 @@ data class Competition(override var id: Long, val name: String, val description:
         if (teams.map { it.number }.contains(team.number)) {
             throw IllegalArgumentException("Team with Number ${team.number} already exists")
         }
-
         teams.add(team)
     }
 
--- backend/src/main/kotlin/dres/data/model/competition/Team.kt
+++ backend/src/main/kotlin/dres/data/model/competition/Team.kt
@@ -1,9 +1,22 @@
 package dres.data.model.competition
 
-import dres.data.model.Entity
-import java.awt.Color
-import java.nio.file.Path
+import java.awt.image.BufferedImage
+import java.io.ByteArrayInputStream
+import javax.imageio.ImageIO
+import javax.xml.bind.DatatypeConverter
 
-data class Team (override var id: Long, val name: String, val number: Int, val color: String, val logoLocation: String) : Entity {
-    fun logo(): Path = Path.of(logoLocation)
+
+data class Team (val number: Int, val name: String, val color: String, val logo: String) {
+    /**
+     * Returns the logo data as [BufferedImage].
+     *
+     * @return [BufferedImage] of the logo.
+     */
+    fun logo(): BufferedImage {
+        val base64Image: String = this.logo.split(",")[1]
+        val imageBytes = DatatypeConverter.parseBase64Binary(base64Image)
+        ByteArrayInputStream(imageBytes).use {
+            return ImageIO.read(it)
+        }
+    }
 }

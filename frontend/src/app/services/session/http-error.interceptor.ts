d30b237f3237bcd8afecdfaa91d18ae6dd294b95
Ralph Gasser <rg@pontius.ch>
2020-03-30 16:56:38 +0200

    Bugfix!

--- frontend/src/app/services/session/http-error.interceptor.ts
+++ frontend/src/app/services/session/http-error.interceptor.ts
@@ -9,11 +9,15 @@ export class HttpErrorInterceptor implements HttpInterceptor {
     constructor(private sessionService: SessionService) { }
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
-            if (this.sessionService.isLoggedIn()) {
-                console.log('Warning: It seems that you were logged out by the server!');
-                this.sessionService.end();
+            if ([401, 403].indexOf(err.status) !== -1) {
+                if (this.sessionService.isLoggedIn()) {
+                    console.log('Warning: It seems that you were logged out by the server!');
+                    this.sessionService.end();
+                }
+                location.reload();
+                const error = err.error.message || err.statusText;
+                return throwError(error);
             }
-            location.reload();
             const error = err.error.message || err.statusText;
             return throwError(error);
         }));

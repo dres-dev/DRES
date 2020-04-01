b339503967dbd30b0554ed4769e2523a035098c7
Ralph Gasser <rg@pontius.ch>
2020-04-01 22:15:51 +0200

    Tasks now have a duration attribute.

--- backend/src/main/kotlin/dres/data/model/competition/Task.kt
+++ backend/src/main/kotlin/dres/data/model/competition/Task.kt
@@ -3,4 +3,4 @@ package dres.data.model.competition
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Task(val name: String, val taskGroup: String, val description: TaskDescription) //TODO maybe normalise taskGroup into its own entity?
+data class Task(val name: String, val taskGroup: String, val description: TaskDescription, val duration: Long = description.taskType.defaultDuration) //TODO maybe normalise taskGroup into its own entity?
--- backend/src/main/kotlin/dres/data/model/competition/TaskType.kt
+++ backend/src/main/kotlin/dres/data/model/competition/TaskType.kt
@@ -1,5 +1,7 @@
 package dres.data.model.competition
 
-enum class TaskType {
-    KIS_VISUAL, KIS_TEXTUAL, AVS
+enum class TaskType(val defaultDuration: Long) {
+    KIS_VISUAL(300_000),
+    KIS_TEXTUAL(420_000),
+    AVS(300_000)
 }

e17341a7e2dc2a5ce11dad1241d20a21d97bdb45
Ralph Gasser <rg@pontius.ch>
2020-04-01 12:45:14 +0200

    Added TODO.

--- backend/src/main/kotlin/dres/data/model/run/Submission.kt
+++ backend/src/main/kotlin/dres/data/model/run/Submission.kt
@@ -5,6 +5,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 sealed class Submission(open val team: Int, open val timestamp: Long, open val name: String, val type: SubmissionType)
 
+
+/** TODO: Rename + rethink structure. */
+
 class AvsSubmission(team: Int, timestamp: Long, name: String): Submission(team, timestamp, name, SubmissionType.AVS)
 
 class KisSubmission(team: Int, timestamp: Long, name: String, val start: Long, val end: Long): Submission(team, timestamp, name, SubmissionType.KIS)

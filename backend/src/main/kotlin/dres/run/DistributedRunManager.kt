5ea1c4c3f59f44620c020208ec3e7674db45390e
Ralph Gasser <rg@pontius.ch>
2020-04-01 22:16:17 +0200

    Completed implementation of first version of DistributedRunManager.

--- backend/src/main/kotlin/dres/run/DistributedRunManager.kt
+++ backend/src/main/kotlin/dres/run/DistributedRunManager.kt
@@ -14,7 +14,15 @@ import java.util.concurrent.locks.ReentrantReadWriteLock
 import kotlin.concurrent.read
 import kotlin.concurrent.write
 
-class DistributedRunManager(competition: Competition, name: String, private val executor: RunExecutor, private val dao: DAO<CompetitionRun>) : RunManager {
+/**
+ * An implementation of [RunManager] aimed at distributed execution having a single DRES Server
+ * instance and multiple viewers connected via WebSocket. Before starting a [Task], all viewer
+ * instances are synchronized.
+ *
+ * @version 1.0
+ * @author Ralph Gasser
+ */
+class DistributedRunManager(competition: Competition, name: String, override val scoreboards: List<Scoreboard>, private val executor: RunExecutor, private val dao: DAO<CompetitionRun>) : RunManager {
 
     /** The [CompetitionRun] capturing the state of this [DistributedRunManager]. */
     private val run = CompetitionRun(-1, name, competition)
@@ -46,11 +54,14 @@ class DistributedRunManager(competition: Competition, name: String, private val 
     /** A lock for state changes to this [DistributedRunManager]. */
     private val stateLock = ReentrantReadWriteLock()
 
-    /** */
+    /** Internal counter used to count the number of WebSocket connected clients. */
     @Volatile
     private var clientCounter = 0
 
-    /** */
+    /**
+     * Internal counter used to count the number ACKs received since the last issuing of a
+     * [ServerMessageType.TASK_PREPARE] message. -1 in cases where no [Task] is running.
+     */
     @Volatile
     private var ackCounter = -1
 
@@ -59,23 +70,14 @@ class DistributedRunManager(competition: Competition, name: String, private val 
         this.run.id = this.dao.append(this.run)
     }
 
-    override val scoreboards: List<Scoreboard>
-        get() = TODO("Not yet implemented")
 
-    /**
-     *
-     */
-    override fun start(): Boolean = this.stateLock.write {
+    override fun start() = this.stateLock.write {
         if (this.status != RunManagerStatus.CREATED) throw IllegalStateException("DistributedRunManager is in status ${this.status} and cannot be started.")
         this.run.start()
         this.status = RunManagerStatus.ACTIVE
         this.executor.broadcastWsMessage(ServerMessage(this.runId, ServerMessageType.COMPETITION_START))
-        return true
     }
 
-    /**
-     *
-     */
     override fun terminate() = this.stateLock.write {
         if (this.status != RunManagerStatus.ACTIVE) throw IllegalStateException("DistributedRunManager is in status ${this.status} and cannot be terminated.")
         this.run.end()
@@ -83,14 +85,24 @@ class DistributedRunManager(competition: Competition, name: String, private val 
         this.executor.broadcastWsMessage(ServerMessage(this.runId, ServerMessageType.COMPETITION_END))
     }
 
-    override fun previousTask() = this.stateLock.write {
+    override fun previousTask(): Boolean = this.stateLock.write {
         val newIndex = this.competition.tasks.indexOf(this.currentTask) - 1
-        this.goToTask(newIndex)
+        return try {
+            this.goToTask(newIndex)
+            true
+        } catch (e: IndexOutOfBoundsException) {
+            false
+        }
     }
 
-    override fun nextTask() = this.stateLock.write {
+    override fun nextTask(): Boolean = this.stateLock.write {
         val newIndex = this.competition.tasks.indexOf(this.currentTask) + 1
-        this.goToTask(newIndex)
+        return try {
+            this.goToTask(newIndex)
+            true
+        } catch (e: IndexOutOfBoundsException) {
+            false
+        }
     }
 
     override fun goToTask(index: Int) = this.stateLock.write {
@@ -120,22 +132,25 @@ class DistributedRunManager(competition: Competition, name: String, private val 
 
         /**  End TaskRun. */
         this.run.currentTask?.end()
+        this.dao.update(this.run)
 
         this.status = RunManagerStatus.ACTIVE
         this.ackCounter = -1
         this.executor.broadcastWsMessage(this.runId, ServerMessage(this.runId, ServerMessageType.TASK_END))
     }
 
-    override fun timeElapsed(): Long = this.stateLock.write {
-        TODO("Not yet implemented")
+    override fun timeElapsed(): Long = this.stateLock.read {
+        if (this.status == RunManagerStatus.RUNNING_TASK) {
+            return (System.currentTimeMillis() - this.run.currentTask!!.started!!)
+        } else {
+            -1L
+        }
     }
-
-
 
     /**
      * Processes WebSocket message received by the [RunExecutor].
      */
-    override fun wsMessageReceived(message: ClientMessage) = this.stateLock.read {
+    override fun wsMessageReceived(message: ClientMessage): Boolean = this.stateLock.read {
         when (message.type) {
             ClientMessageType.ACK -> {
                 if (this.status == RunManagerStatus.PREPARING_TASK) {
@@ -145,46 +160,76 @@ class DistributedRunManager(competition: Competition, name: String, private val 
             ClientMessageType.REGISTER -> this.clientCounter += 1
             ClientMessageType.UNREGISTER -> this.clientCounter -= 1
         }
+        return true
     }
 
     /**
-     * Processes [Submission]s received by ...
+     * Processes incoming [Submission]s. If a [Task] is running then that [Submission] will usually
+     * be associated with the current [TaskRun].
+     *
+     * This method will not throw an exception and instead return false if a [Submission] was
+     * ignored for whatever reason (usually a state mismatch). It is up to the caller to re-invoke
+     * this method again.
+     *
+     * @param sub [Submission] that should be registered.
      */
-    override fun postSubmission(sub: Submission) = this.stateLock.read {
+    override fun postSubmission(sub: Submission): Boolean = this.stateLock.read {
         if (this.status == RunManagerStatus.RUNNING_TASK) {
+            /* Register submission. */
+            this.run.currentTask?.addSubmission(sub)
+
+            /* Update scoreboards. */
+            this.scoreboards.forEach { it.update() }
 
             /* Inform clients about update. */
             this.executor.broadcastWsMessage(ServerMessage(this.runId, ServerMessageType.TASK_UPDATED))
+
+            return true
+        } else {
+            return false
         }
     }
 
     override fun run() {
-        /** */
-        loop@while (true) {
+        /** WAi*/
+        while (true) {
             if (this.status != RunManagerStatus.CREATED) {
-                break@loop
+                break
             }
             Thread.onSpinWait()
         }
 
-        /** Handles the activity part of the DistributedRunManager.*/
+        /** Handles the activity part of the DistributedRunManager (status = ACTIVE).*/
         while (this.status == RunManagerStatus.ACTIVE) {
-            /** Handles the preparation period of the DistributedRunManager. */
+
+            /** Handles the preparation period of the DistributedRunManager (status = PREPARING_TASK). */
             while (this.status == RunManagerStatus.PREPARING_TASK) {
                 if (this.ackCounter >= this.clientCounter) {
                     this.stateLock.write {
-                        this.status = RunManagerStatus.RUNNING_TASK
                         this.run.currentTask?.start()
+                        this.status = RunManagerStatus.RUNNING_TASK
                     }
                     this.executor.broadcastWsMessage(ServerMessage(this.runId, ServerMessageType.TASK_START))
                     break
                 }
-                Thread.onSpinWait()
+
+                /** Sleep for 250ms. */
+                Thread.sleep(250)
             }
 
-            /** Handles the task execution period of the DistributedRunManager. */
+            /** Handles the task execution period of the DistributedRunManager (status = RUNNING_TASK). */
             while (this.status == RunManagerStatus.RUNNING_TASK) {
-                TODO()
+                if ((System.currentTimeMillis() - this.run.currentTask!!.started!!) >= this.run.currentTask!!.task.duration) {
+                    this.stateLock.write {
+                        this.run.currentTask!!.end()
+                        this.status = RunManagerStatus.ACTIVE
+                    }
+                    this.dao.update(this.run)
+                    this.executor.broadcastWsMessage(ServerMessage(this.runId, ServerMessageType.TASK_END))
+                }
+
+                /** Sleep for 250ms. */
+                Thread.sleep(250)
             }
 
             Thread.onSpinWait()

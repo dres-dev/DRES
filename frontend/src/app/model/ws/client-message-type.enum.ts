27eb3e665992284e3e537b631d06cfe7aac93095
Ralph Gasser <rg@pontius.ch>
2020-04-08 10:07:30 +0200

    Added data model for WebSocket message exchange.

---
+++ frontend/src/app/model/ws/client-message-type.enum.ts
@@ -0,0 +1,12 @@
+export namespace ClientMessageType {
+    export type ClientMessageTypeEnum = 'ACK' | 'REGISTER' | 'UNREGISTER';
+    export const ClientMessageTypeEnum = {
+        COMPETITION_START: 'COMPETITION_START' as ClientMessageTypeEnum,
+        COMPETITION_UPDATE: 'COMPETITION_UPDATE' as ClientMessageTypeEnum,
+        COMPETITION_END: 'COMPETITION_END' as ClientMessageTypeEnum,
+        TASK_PREPARE: 'TASK_PREPARE' as ClientMessageTypeEnum,
+        TASK_START: 'TASK_START' as ClientMessageTypeEnum,
+        TASK_UPDATED: 'TASK_UPDATED' as ClientMessageTypeEnum,
+        TASK_END: 'TASK_END' as ClientMessageTypeEnum
+    };
+}
---
+++ frontend/src/app/model/ws/server-message-type.enum.ts
@@ -0,0 +1,13 @@
+// tslint:disable-next-line:no-namespace
+export namespace ServerMessageType {
+    export type ServerMessageTypeEnum = 'COMPETITION_START' | 'COMPETITION_UPDATE' | 'COMPETITION_END' | 'TASK_PREPARE' | 'TASK_START' | 'TASK_UPDATED' | 'TASK_END';
+    export const ServerMessageTypeEnum = {
+        COMPETITION_START: 'COMPETITION_START' as ServerMessageTypeEnum,
+        COMPETITION_UPDATE: 'COMPETITION_UPDATE' as ServerMessageTypeEnum,
+        COMPETITION_END: 'COMPETITION_END' as ServerMessageTypeEnum,
+        TASK_PREPARE: 'TASK_PREPARE' as ServerMessageTypeEnum,
+        TASK_START: 'TASK_START' as ServerMessageTypeEnum,
+        TASK_UPDATED: 'TASK_UPDATED' as ServerMessageTypeEnum,
+        TASK_END: 'TASK_END' as ServerMessageTypeEnum
+    };
+}

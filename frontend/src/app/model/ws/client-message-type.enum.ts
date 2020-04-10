65ac5b4892126fbd654c3adfd1c81db0ee69dd8e
Ralph Gasser <rg@pontius.ch>
2020-04-10 13:20:05 +0200

    Bugfix!

--- frontend/src/app/model/ws/client-message-type.enum.ts
+++ frontend/src/app/model/ws/client-message-type.enum.ts
@@ -1,13 +1,9 @@
 export namespace ClientMessageType {
     export type ClientMessageTypeEnum = 'ACK' | 'REGISTER' | 'UNREGISTER';
     export const ClientMessageTypeEnum = {
-        COMPETITION_START: 'COMPETITION_START' as ClientMessageTypeEnum,
-        COMPETITION_UPDATE: 'COMPETITION_UPDATE' as ClientMessageTypeEnum,
-        COMPETITION_END: 'COMPETITION_END' as ClientMessageTypeEnum,
-        TASK_PREPARE: 'TASK_PREPARE' as ClientMessageTypeEnum,
-        TASK_START: 'TASK_START' as ClientMessageTypeEnum,
-        TASK_UPDATED: 'TASK_UPDATED' as ClientMessageTypeEnum,
-        TASK_END: 'TASK_END' as ClientMessageTypeEnum
+        ACK: 'ACK' as ClientMessageTypeEnum,
+        REGISTER: 'REGISTER' as ClientMessageTypeEnum,
+        UNREGISTER: 'UNREGISTER' as ClientMessageTypeEnum,
     };
     export const ClientMessageTypes: ClientMessageTypeEnum[] = ['ACK', 'REGISTER', 'UNREGISTER'];
 }

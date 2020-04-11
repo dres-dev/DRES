25409c973dec817e8a77a13f358c9a882a82bdd1
Luca Rossetto <lucaro@lucaro.ch>
2020-04-11 17:17:36 +0200

    Revert "Removed coroutines from submission validation and made result explicitly deferred"
    This reverts commit b4bc670c12cb52ca56bb762d875d08f12ca22247.

--- backend/src/main/kotlin/dres/run/validate/SubmissionValidator.kt
+++ backend/src/main/kotlin/dres/run/validate/SubmissionValidator.kt
@@ -3,13 +3,12 @@ package dres.run.validate
 import dres.data.model.competition.TaskDescription
 import dres.data.model.run.Submission
 import dres.data.model.run.SubmissionStatus
-import kotlinx.coroutines.Deferred
 
 /**
  *  Checks if a submission is correct
  */
 interface SubmissionValidator<S : Submission, T: TaskDescription> {
 
-    fun validate(submission: S, task: T): Deferred<SubmissionStatus>
+    suspend fun validate(submission: S, task: T): SubmissionStatus
 
 }

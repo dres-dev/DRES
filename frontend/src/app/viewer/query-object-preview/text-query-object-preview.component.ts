3844e61c8e90d7ac75fc97473b454ebdb3916777
Ralph Gasser <rg@pontius.ch>
2020-04-29 22:24:55 +0200

    Incremental textual query hints are now displayed by replacing the preceding text instead of building up a growing list.

--- frontend/src/app/viewer/query-object-preview/text-query-object-preview.component.ts
+++ frontend/src/app/viewer/query-object-preview/text-query-object-preview.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {TextQueryDescription} from '../../../../openapi';
+import {map, withLatestFrom} from 'rxjs/operators';
 
 @Component({
     selector: 'app-text-query-object-preview',
@@ -11,13 +12,19 @@ export class TextQueryObjectPreviewComponent implements OnInit, OnDestroy {
     @Input() queryObject: Observable<TextQueryDescription>;
     @Input() timeElapsed: Observable<number>;
 
+    currentText: Observable<string>;
+
     /** Font size in em. TODO: Make configurable. */
     fontSize = 2.0;
 
-
-    ngOnDestroy(): void {
+    ngOnInit(): void {
+        this.currentText = this.timeElapsed.pipe(
+            withLatestFrom(this.queryObject),
+            map(([time, query]) => query.text.filter(t => t.showAfter < time).pop()?.text),
+        );
     }
 
-    ngOnInit(): void {
+    ngOnDestroy(): void {
+
     }
 }

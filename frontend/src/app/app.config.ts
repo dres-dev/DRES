289ea6277d6cc2b806ab11752e0e52a2cc973b15
Ralph Gasser <rg@pontius.ch>
2020-05-07 15:58:29 +0200

    AppConfig now offers a facility to resolve URLs relative to the OpenAPI endpoint.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -6,6 +6,27 @@ import {HttpClient} from '@angular/common/http';
 export class AppConfig {
     static settings: IConfig;
     constructor(private http: HttpClient) {}
+
+    /**
+     * Returns the IConfig object for this application
+     *
+     * @return Current IConfig instance.
+     */
+    get config(): IConfig {
+        return AppConfig.settings;
+    }
+
+    /**
+     * Resolves the given path given the global API URL. That is, it returns the URL of the
+     * form http://[host]:[port]/api/[path]
+     *
+     * @param path The path to resolve.
+     * @return The full URL to the API endpoint.
+     */
+    public resolveApiUrl(path: string) {
+        return `${AppConfig.settings.endpoint.tls ? 'https://' : 'http://'}${AppConfig.settings.endpoint.host}:${AppConfig.settings.endpoint.port}/api${path.startsWith('/') ? '' : '/'}${path}`;
+    }
+
     load() {
         const jsonFile = 'config.json';
         return new Promise<void>((resolve, reject) => {

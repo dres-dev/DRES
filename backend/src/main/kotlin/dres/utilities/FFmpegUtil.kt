54f57da5c48ccfc950d9d7b83a963a217cdc2505
Luca Rossetto <lucaro@lucaro.ch>
2020-05-14 18:20:18 +0200

    Added mechanism to limit number of concurrent calls to ffmpeg

--- backend/src/main/kotlin/dres/utilities/FFmpegUtil.kt
+++ backend/src/main/kotlin/dres/utilities/FFmpegUtil.kt
@@ -6,6 +6,7 @@ import com.github.kokorin.jaffree.ffmpeg.UrlOutput
 import dres.data.model.competition.interfaces.MediaSegmentTaskDescription
 import java.io.File
 import java.nio.file.Path
+import java.util.concurrent.Semaphore
 
 object FFmpegUtil {
 
@@ -21,6 +22,8 @@ object FFmpegUtil {
         
     }") //TODO make configurable
 
+    private val semaphore = Semaphore(16) //TODO make number configurable
+
     private fun toMillisecondTimeStamp(ms: Long): String {
         val hours = ms / (1000 * 3600)
         val minutes = (ms % (1000 * 3600)) / (60_000)
@@ -32,30 +35,40 @@ object FFmpegUtil {
 
 
     fun extractFrame(video: Path, timecode: String, outputImage: Path) {
-        FFmpeg.atPath(ffmpegBin)
-            .addInput(UrlInput.fromPath(video))
-            .addOutput(UrlOutput.toPath(outputImage))
-            .setOverwriteOutput(true)
-            .addArguments("-ss", timecode)
-            .addArguments("-vframes", "1")
-            .execute()
+        try {
+            semaphore.acquire()
+            FFmpeg.atPath(ffmpegBin)
+                    .addInput(UrlInput.fromPath(video))
+                    .addOutput(UrlOutput.toPath(outputImage))
+                    .setOverwriteOutput(true)
+                    .addArguments("-ss", timecode)
+                    .addArguments("-vframes", "1")
+                    .executeAsync()
+        } finally {
+            semaphore.release()
+        }
     }
 
     fun extractFrame(video: Path, ms: Long, outputImage: Path) = extractFrame(video, toMillisecondTimeStamp(ms), outputImage)
 
     fun extractSegment(video: Path, startTimecode: String, endTimecode: String, outputVideo: Path) {
-        FFmpeg.atPath(ffmpegBin)
-                .addInput(UrlInput.fromPath(video))
-                .addOutput(UrlOutput.toPath(outputVideo))
-                .setOverwriteOutput(true)
-                .addArguments("-ss", startTimecode)
-                .addArguments("-to", endTimecode)
-                .addArguments("-c:v", "libx264")
-                .addArguments("-c:a", "aac")
-                .addArguments("-b:v", "2000k")
-                .addArguments("-tune", "zerolatency")
-                .addArguments("-preset", "slow")
-                .execute()
+        try {
+            semaphore.acquire()
+            FFmpeg.atPath(ffmpegBin)
+                    .addInput(UrlInput.fromPath(video))
+                    .addOutput(UrlOutput.toPath(outputVideo))
+                    .setOverwriteOutput(true)
+                    .addArguments("-ss", startTimecode)
+                    .addArguments("-to", endTimecode)
+                    .addArguments("-c:v", "libx264")
+                    .addArguments("-c:a", "aac")
+                    .addArguments("-b:v", "2000k")
+                    .addArguments("-tune", "zerolatency")
+                    .addArguments("-preset", "slow")
+                    .execute()
+        } finally {
+            semaphore.release()
+        }
     }
 
     fun prepareMediaSegmentTask(description: MediaSegmentTaskDescription, collectionBasePath: String, cacheLocation: File) {

d4994217e8eaa0321d1f9f4c2575238e756e4b46
Luca Rossetto <lucaro@lucaro.ch>
2020-05-21 17:44:19 +0200

    Added logging to ErrorStatusException

--- backend/src/main/kotlin/dres/api/rest/types/status/ErrorStatusException.kt
+++ backend/src/main/kotlin/dres/api/rest/types/status/ErrorStatusException.kt
@@ -1,8 +1,17 @@
 package dres.api.rest.types.status
 
-import java.lang.Exception
+import org.slf4j.LoggerFactory
 
 data class ErrorStatusException(val statusCode: Int, val status: String) : Exception(status) {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(this.javaClass)
+    }
+
+    init {
+        logger.info("ErrorStatusException with code $statusCode and message '$status' thrown by ${stackTrace.first()}")
+    }
+
     val errorStatus: ErrorStatus
         get() = ErrorStatus(status)
 }

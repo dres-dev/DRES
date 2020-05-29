5110fbf06142387a81ab8faf50abb974a0205096
Luca Rossetto <lucaro@lucaro.ch>
2020-05-29 18:11:01 +0200

    Added update mechanism to DaoIndexer

--- backend/src/main/kotlin/dres/data/dbo/DaoIndexer.kt
+++ backend/src/main/kotlin/dres/data/dbo/DaoIndexer.kt
@@ -1,9 +1,9 @@
 package dres.data.dbo
 
 import dres.data.model.Entity
-import java.util.concurrent.locks.ReentrantReadWriteLock
-import kotlin.concurrent.read
-import kotlin.concurrent.write
+import dres.utilities.extensions.optimisticRead
+import dres.utilities.extensions.write
+import java.util.concurrent.locks.StampedLock
 
 /**
  * Wrapper for DAO which enables index-based access
@@ -12,26 +12,41 @@ class DaoIndexer<T: Entity, K>(private val dao: DAO<T>, private val keyTransform
 
     private val index: MutableMap<K, MutableList<Long>> = mutableMapOf()
 
-    private val updateLock = ReentrantReadWriteLock()
+    private val lock = StampedLock()
 
     init {
         //load DAO to index
         rebuild()
+        dao.addIndexer(this)
     }
 
     /**
      * rebuilds the index
      */
-    fun rebuild() = updateLock.write {
+    fun rebuild() = lock.write {
         val map = dao.groupBy( keyTransform ).mapValues { it.value.map { e -> e.id }.toMutableList() }
         index.clear()
         index.putAll(map)
     }
 
-    operator fun get(key: K): List<T> = updateLock.read {
+    operator fun get(key: K): List<T> = lock.optimisticRead {
         index[key]?.mapNotNull { dao[it] } ?: emptyList()
     }
 
-    fun keys(): Set<K> = updateLock.read { index.keys }
+    fun keys(): Set<K> = lock.optimisticRead { index.keys }
+
+
+    internal fun delete(value: T) = lock.write {
+        index[keyTransform(value)]?.remove(value.id)
+    }
+
+    internal fun append(value: T) = lock.write {
+        val key = keyTransform(value)
+        if (!index.containsKey(key)) {
+            index[key] = mutableListOf(value.id)
+        } else {
+            index[key]!!.add(value.id)
+        }
+    }
 
 }

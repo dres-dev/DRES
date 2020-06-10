9ecdaf12e81291f6714ac40c7762c6727c4bbb58
Loris Sauter <loris.sauter@unibas.ch>
2020-06-10 14:17:10 +0200

    Changed to consistently, singular command names (CLI)

--- backend/src/main/kotlin/dres/api/cli/CompetitionRunCommand.kt
+++ backend/src/main/kotlin/dres/api/cli/CompetitionRunCommand.kt
@@ -17,7 +17,7 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
 
-class CompetitionRunCommand(internal val runs: DAO<CompetitionRun>) : NoOpCliktCommand(name = "runs") {
+class CompetitionRunCommand(internal val runs: DAO<CompetitionRun>) : NoOpCliktCommand(name = "run") {
 
     init {
         subcommands(OngoingCompetitionRunsCommand(), ListCompetitionRunsCommand(), DeleteRunCommand(), ExportRunCommand(), CompetitionRunsHistoryCommand(), ResetSubmissionStatusCommand())

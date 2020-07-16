0b63f5b7aebf602b0ec3b457e0a0161f9801ecca
Luca Rossetto <lucaro@lucaro.ch>
2020-07-16 20:37:47 +0200

    Added default values to all fields of ParticipantLog data classes

--- backend/src/main/kotlin/dres/data/model/log/ParticipantLog.kt
+++ backend/src/main/kotlin/dres/data/model/log/ParticipantLog.kt
@@ -5,19 +5,31 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
 
-data class QueryEvent(val timestamp: Long, val category: String, val type: List<String>, val value: String?)
+data class QueryEvent(val timestamp: Long = -1, val category: String = "", val type: List<String> = emptyList(), val value: String?)
 
-data class QueryEventLog internal constructor(val team: Int, val member: Long, val timestamp: Long, val events: List<QueryEvent>, val serverTimeStamp: Long, val type:String) {
+data class QueryEventLog internal constructor(val team: Int = -1, val member: Long = -1,
+                                              val timestamp: Long = -1,
+                                              val events: List<QueryEvent> = emptyList(),
+                                              val serverTimeStamp: Long = System.currentTimeMillis(),
+                                              val type: String = "") {
     @JsonCreator
-    constructor(@JsonProperty("teamId") team: Int, @JsonProperty("memberId") member: Long, @JsonProperty("timestamp") timestamp: Long, @JsonProperty("events") events: List<QueryEvent>, @JsonProperty("type" )type: String) : this(team, member, timestamp, events, System.currentTimeMillis(), type)
+    constructor(
+            @JsonProperty("teamId") team: Int = -1,
+            @JsonProperty("memberId") member: Long = -1,
+            @JsonProperty("timestamp") timestamp: Long = -1,
+            @JsonProperty("events") events: List<QueryEvent> = emptyList(),
+            @JsonProperty("type" )type: String = ""
+    ) : this(team, member, timestamp, events, System.currentTimeMillis(), type)
 }
 
-data class QueryResult(val video: String, val shot: Int? = null, val frame: Int? = null, val score: Double? = null, val rank: Int? = null)
+data class QueryResult(val video: String = "", val shot: Int? = null, val frame: Int? = null, val score: Double? = null, val rank: Int? = null)
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class QueryResultLog internal constructor(val team: Int, val member: Long, val timestamp: Long, val values: List<String>, val usedCategories: List<String>,
-                                               val usedTypes: List<String>, val sortType: List<String>,
-                                               val resultSetAvailability: String, val results: List<QueryResult>, val serverTimeStamp: Long){
+data class QueryResultLog internal constructor(val team: Int = -1, val member: Long = -1, val timestamp: Long = -1,
+                                               val values: List<String> = emptyList(), val usedCategories: List<String> = emptyList(),
+                                               val usedTypes: List<String> = emptyList(), val sortType: List<String> = emptyList(),
+                                               val resultSetAvailability: String = "", val results: List<QueryResult> = emptyList(),
+                                               val serverTimeStamp: Long = System.currentTimeMillis()){
     @JsonCreator constructor(
             @JsonProperty("teamId") team: Int,
             @JsonProperty("memberId") member: Long,

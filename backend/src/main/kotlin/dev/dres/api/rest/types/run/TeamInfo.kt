67b53cfdad69444adef285395eb0089c1122bc05
Ralph Gasser <rg@pontius.ch>
2020-11-20 15:45:45 +0100

    TeamInfo now also contains the UID.

--- backend/src/main/kotlin/dev/dres/api/rest/types/run/TeamInfo.kt
+++ backend/src/main/kotlin/dev/dres/api/rest/types/run/TeamInfo.kt
@@ -9,8 +9,8 @@ import dev.dres.data.model.run.CompetitionRun
  * and other optimizations.
  *
  * @author Ralph Gasser
- * @version 1.0.1
+ * @version 1.0.2
  */
-data class TeamInfo(val name: String, val color: String, val logoId: String) {
-    constructor(team: Team) : this(team.name, team.color, team.logoId.string)
+data class TeamInfo(val uid: String, val name: String, val color: String, val logoId: String) {
+    constructor(team: Team) : this(team.uid.string, team.name, team.color, team.logoId.string)
 }

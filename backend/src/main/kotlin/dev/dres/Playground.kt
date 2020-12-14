61c2fbdfcdbedd4f2be1733d2fd4c7f8e7ffcab4
Luca Rossetto <lucaro@lucaro.ch>
2020-12-14 17:55:30 +0100

    Fixed UID formatting issue in SubmissionStatisticsHandler, closes #222

--- backend/src/main/kotlin/dev/dres/Playground.kt
+++ backend/src/main/kotlin/dev/dres/Playground.kt
@@ -1,7 +1,15 @@
 package dev.dres
 
-//import dev.dres.data.model.UID
-//import dev.dres.utilities.extensions.UID
+import dev.dres.data.dbo.DAO
+import dev.dres.data.model.UID
+import dev.dres.data.model.competition.TaskDescriptionTarget
+import dev.dres.data.serializers.AuditLogEntrySerializer
+import dev.dres.data.serializers.CompetitionSerializer
+import dev.dres.data.serializers.MediaItemSerializer
+import dev.dres.run.audit.SubmissionAuditLogEntry
+import dev.dres.run.audit.TaskStartAuditLogEntry
+import java.io.File
+import java.nio.file.Paths
 
 
 object Playground {
@@ -9,35 +17,81 @@ object Playground {
     @JvmStatic
     fun main(args: Array<String>) {
 
-//        val ffmpegBin = Path.of("ext/ffmpeg/")
-//
-//        val fileEndings = listOf("mp4", "mov")
-//
-//        val startFolder = File("Z:\\Datasets\\V3C\\V3C1\\videos").walkTopDown().filter { it.isFile && it.extension in fileEndings }.forEach { videoFile ->
-//
-//            println("found $videoFile")
-//
-//            val ffprobe = FFprobe.atPath(ffmpegBin)
-//                    .setInput(videoFile.toPath())
-//                    .setShowStreams(true)
-//                    .setSelectStreams(StreamType.VIDEO)
-//                    .execute()
-//
-//            val stream = ffprobe.streams.first()
-//
-//            val fps = stream.rFrameRate ?: stream.avgFrameRate
-//            val fpsd = fps.toDouble()
-//
-//            val duration = stream.getDuration(TimeUnit.MILLISECONDS)
-//
-//            println("found duration $duration and frame rate $fpsd for $videoFile")
-//            println()
-//
-//        }
+
+        val audit = DAO(Paths.get("C:\\Users\\Lucaro\\Downloads\\LSC2020\\data_branch/auditLog.db"), AuditLogEntrySerializer)
+
+        val mediaItems = DAO(Paths.get("C:\\Users\\Lucaro\\Downloads\\LSC2020\\data_branch/mediaItems.db"), MediaItemSerializer)
+        val competitionSerializer = CompetitionSerializer(mediaItems)
+
+        val competitions = DAO(Paths.get("C:\\Users\\Lucaro\\Downloads\\LSC2020\\data_branch/competitions.db"), competitionSerializer)
 
 
-       // println(UID.EMPTY.string.UID())
+        val competition = competitions[UID("db2ef71a-df42-4fbd-a2ea-2e1c8b393edd")]!!
 
+        val targets = competition.tasks.map {
+            it.name to (it.target as TaskDescriptionTarget.MultipleMediaItemTarget).items.map { it.name }
+        }.toMap()
+
+        val teams = competition.teams.map { it.name }
+
+//        println(audit.find { it.id.string == "b54fc9dd-5a86-4c92-be7b-7642b35c887b" }!!.timestamp)
+//
+//        return
+
+        val events = audit.filter { it.timestamp > 1604053744241 }.sortedBy { it.timestamp }
+
+        val taskStartTimes = mutableMapOf<String, Long>()
+
+        val submissionWriter = File("submissions.csv").printWriter()
+        val taskStartWriter = File("taskStart.csv").printWriter()
+
+        submissionWriter.println("timestamp,task,time,team,member,session,item,correct")
+        taskStartWriter.println("timestamp,task")
+
+        for (event in events) {
+
+            when(event) {
+
+                is TaskStartAuditLogEntry -> {
+
+                    taskStartTimes[event.taskName] = event.timestamp
+                    taskStartWriter.println("${event.timestamp},${event.taskName}")
+
+                }
+
+
+                is SubmissionAuditLogEntry -> {
+
+                    val taskName = event.taskName
+
+                    if (!targets.containsKey(taskName)){
+                        println("ignoring $taskName")
+                        continue
+                    }
+
+                    val time = event.submission.timestamp - taskStartTimes[taskName]!!
+                    val item = event.submission.item.name
+                    val correct = (targets[taskName] ?:
+                    error("$taskName not found")).contains(item)
+
+
+
+                    //submissionWriter.println("${event.timestamp},$taskName,$time,${teams[event.submission.teamId]},${event.submission.member.string},${event.user},$item,$correct")
+
+                }
+
+                else -> {
+                    println("ignoring $event")
+                }
+
+            }
+
+        }
+
+        submissionWriter.flush()
+        submissionWriter.close()
+        taskStartWriter.flush()
+        taskStartWriter.close()
 
 
     }

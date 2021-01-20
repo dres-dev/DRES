4e435e7c6c3454ed46a9200eded70bd77f145945
Ralph Gasser <rg@pontius.ch>
2021-01-20 14:17:11 +0100

    Fixed issue in EndTaskUpdatable that would cause the first tasks in a group to end prematurely.

--- backend/src/main/kotlin/dev/dres/run/updatables/EndTaskUpdatable.kt
+++ backend/src/main/kotlin/dev/dres/run/updatables/EndTaskUpdatable.kt
@@ -1,55 +1,37 @@
 package dev.dres.run.updatables
 
-import dev.dres.data.model.UID
 import dev.dres.data.model.competition.TaskType
 import dev.dres.data.model.run.SubmissionStatus
 import dev.dres.run.RunManager
 import dev.dres.run.RunManagerStatus
+import java.util.concurrent.atomic.AtomicInteger
 
 class EndTaskUpdatable(private val run: RunManager) : Updatable {
 
     override val phase: Phase = Phase.MAIN
 
-    private var running = true
-    private var lastKnownTaskId: UID = UID.EMPTY
+    /** Number of submissions seen during the last update. */
+    private var submissions = AtomicInteger(0)
 
     override fun update(status: RunManagerStatus) {
-
-        val limitingFilter = run.currentTaskRun?.task?.taskType?.filter?.find{ it.option == TaskType.SubmissionFilterType.LIMIT_CORRECT_PER_TEAM }
-            ?: /* no filter */
-            return
-
+        val limitingFilter = run.currentTaskRun?.task?.taskType?.filter?.find{ it.option == TaskType.SubmissionFilterType.LIMIT_CORRECT_PER_TEAM } ?: return
         val limit = limitingFilter.parameters.getOrDefault("limit", "1").toIntOrNull() ?: 1
+        if (this.run.timeLeft() > 0) {
+            val taskRun = this.run.currentTaskRun
+            if (taskRun != null && this.submissions.getAndSet(taskRun.submissions.size) < taskRun.submissions.size) {
+                /* Determine of all teams have submitted . */
+                val allDone = this.run.competitionDescription.teams.all { team ->
+                    run.submissions.count { it.teamId == team.uid && it.status == SubmissionStatus.CORRECT  } >= limit
+                }
 
-        /* Do we know the current task? */
-        if (lastKnownTaskId == run.currentTask?.id) {
-            /* are we still running ? */
-            if (!running) {
-                /* no, so we do not update anymore */
-                /* i.e. the task was just finished because of us */
-                return
+                /* Do all teams have reached the limit of correct submissions ? */
+                if (allDone) {
+                    this.run.abortTask()
+                    this.submissions.set(0)
+                }
             }
-        } else {
-            /* a new task we do not know, so we want to run again */
-            running = true
         }
-        /* let's keep track of the current task, we do this always as we are not specifically called upon task change */
-        lastKnownTaskId = run.currentTask?.id!!
-
-        /* fetch the correct submissions teams */
-        val correctSubmissionTeams = run.currentTaskRun!!.submissions.filter {
-            it.status == SubmissionStatus.CORRECT
-        }.groupBy { it.teamId }.values.map { it.size }
-
-        /* Do all teams have reached the limit of correct submissions ? */
-        if (run.timeLeft() > 5000 &&
-                correctSubmissionTeams.all { it >= limit }) {
-            run.adjustDuration(1 - (run.timeLeft().toInt()) / 1000) /* run.timeLeft() is ms */
-            running = false /* do not update anymore, as the task is over now */
-        }
-
-
     }
 
-    override fun shouldBeUpdated(status: RunManagerStatus): Boolean = status == RunManagerStatus.RUNNING_TASK
+    override fun shouldBeUpdated(status: RunManagerStatus): Boolean = (status == RunManagerStatus.RUNNING_TASK)
 }

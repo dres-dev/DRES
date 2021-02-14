6050e6b1582dc12b589406fe2df0fabf05d095b0
Luca Rossetto <lucaro@lucaro.ch>
2021-02-14 19:10:14 +0100

    Removed internal fields from openapi spec

--- backend/src/main/kotlin/dev/dres/data/model/log/ParticipantLog.kt
+++ backend/src/main/kotlin/dev/dres/data/model/log/ParticipantLog.kt
@@ -1,5 +1,6 @@
 package dev.dres.data.model.log
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonSetter
 import com.fasterxml.jackson.annotation.Nulls
@@ -9,26 +10,42 @@ enum class QueryEventCategory {
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class QueryEvent(val timestamp: Long = -1,
-                      val category: QueryEventCategory = QueryEventCategory.OTHER,
-                      val type: String = "",
-                      val value: String = "")
+data class QueryEvent(
+    val timestamp: Long = -1,
+    val category: QueryEventCategory = QueryEventCategory.OTHER,
+    val type: String = "",
+    val value: String = ""
+)
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class QueryEventLog internal constructor(val timestamp: Long = -1,
-                                              @field:JsonSetter(contentNulls = Nulls.FAIL) val events: List<QueryEvent> = emptyList(),
-                                              internal val serverTimeStamp: Long = System.currentTimeMillis())
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class QueryResult(val item: String = "",
-                       val segment: Int? = null,
-                       val frame: Int? = null,
-                       val score: Double? = null,
-                       val rank: Int? = null)
+data class QueryEventLog internal constructor(
+    val timestamp: Long = -1,
+    @field:JsonSetter(contentNulls = Nulls.FAIL)
+    val events: List<QueryEvent> = emptyList(),
+    @field:JsonIgnore
+    @get:JsonIgnore
+    internal val serverTimeStamp: Long = System.currentTimeMillis()
+)
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class QueryResultLog internal constructor(val timestamp: Long = -1,
-                                               val sortType: String = "",
-                                               val resultSetAvailability: String = "",
-                                               @field:JsonSetter(contentNulls = Nulls.FAIL)val results: List<QueryResult> = emptyList(),
-                                               @field:JsonSetter(contentNulls = Nulls.FAIL)val events: List<QueryEvent> = emptyList(),
-                                               internal val serverTimeStamp: Long = System.currentTimeMillis())
+data class QueryResult(
+    val item: String = "",
+    val segment: Int? = null,
+    val frame: Int? = null,
+    val score: Double? = null,
+    val rank: Int? = null
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class QueryResultLog internal constructor(
+    val timestamp: Long = -1,
+    val sortType: String = "",
+    val resultSetAvailability: String = "",
+    @field:JsonSetter(contentNulls = Nulls.FAIL)
+    val results: List<QueryResult> = emptyList(),
+    @field:JsonSetter(contentNulls = Nulls.FAIL)
+    val events: List<QueryEvent> = emptyList(),
+    @field:JsonIgnore
+    @get:JsonIgnore
+    internal val serverTimeStamp: Long = System.currentTimeMillis()
+)

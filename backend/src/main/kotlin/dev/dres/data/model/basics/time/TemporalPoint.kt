c5aae81dd1bb83d4839216dce0c4d4d6d2ef4e25
Luca Rossetto <lucaro@lucaro.ch>
2021-06-11 16:44:05 +0200

    Started re-implementation of TemporalPoint and related logic

--- backend/src/main/kotlin/dev/dres/data/model/basics/time/TemporalPoint.kt
+++ backend/src/main/kotlin/dev/dres/data/model/basics/time/TemporalPoint.kt
@@ -1,14 +1,70 @@
 package dev.dres.data.model.basics.time
 
+import dev.dres.data.model.basics.media.MediaItem
+import dev.dres.data.model.basics.media.PlayableMediaItem
+import java.lang.IllegalArgumentException
+
 /**
  * Notion of a [TemporalPoint] within a [MediaItem] that exhibits temporal development (e.g. [VideoItem]).
  *
- * @author Ralph Gasser
- * @version 1.0.1
+ * @version 2.0
  *
- * @param value Value of the [TemporalPoint]
- * @param unit Unit of the [TemporalPoint]
  */
-data class TemporalPoint constructor(val value: Double, val unit: TemporalUnit) {
-    fun niceText(): String = "$value ${unit.name.toLowerCase()}"
+sealed class TemporalPoint {
+    abstract fun niceText(): String
+    abstract fun toMilliseconds(): Long
+}
+
+data class FrameTemporalPoint(val frame: Int, val framesPerSecond: Float) : TemporalPoint(){
+    constructor(frame: Int, item: PlayableMediaItem) : this(frame, item.fps)
+
+    companion object {
+        inline fun toMilliseconds(frame: Int, framesPerSecond: Float) : Long = (frame / framesPerSecond * 1000f).toLong()
+    }
+
+    override fun niceText(): String = "FrameTemporalPoint(Frame $frame @ $framesPerSecond fps)"
+
+    override fun toMilliseconds(): Long = toMilliseconds(frame, framesPerSecond)
+}
+
+data class MilliSecondTemporalPoint(val millisecond: Long) : TemporalPoint() {
+    override fun niceText(): String = "MilliSecondTemporalPoint($millisecond)"
+
+    override fun toMilliseconds(): Long = millisecond
+}
+
+data class TimeCodeTemporalPoint(val timecode: String, val framesPerSecond: Float) : TemporalPoint(){
+    constructor(timecode: String, item: PlayableMediaItem) : this(timecode, item.fps)
+
+    companion object {
+        private val timecodeRegex = "^\\s*(?:(?:(?:(\\d+):)?([0-5]?\\d):)?([0-5]?\\d):)?(\\d+)\\s*\$".toRegex()
+
+        private const val msPerHour: Long = 3_600_000
+        private const val msPerMinute: Long = 60_000
+
+        /**
+         * Transforms a time code of the form HH:MM:SS:FF to milliseconds
+         * @return time in milliseconds or null if the input is not a valid time code
+         */
+        fun timeCodeToMilliseconds(timecode: String, fps: Float = 24.0f): Long? {
+
+            val matches = timecodeRegex.matchEntire(timecode) ?: return null
+
+            val hours = matches.groups[1]?.value?.toLong() ?: 0
+            val minutes = matches.groups[2]?.value?.toLong() ?: 0
+            val seconds = matches.groups[3]?.value?.toLong() ?: 0
+            val frames = matches.groups[4]?.value?.toLong() ?: 0
+
+            return hours * msPerHour + minutes * msPerMinute + seconds * 1000 + (1000 * frames / fps).toLong()
+        }
+
+        fun timeCodeToMilliseconds(timecode: String, item: PlayableMediaItem): Long? = timeCodeToMilliseconds(timecode, item.fps)
+    }
+
+    private val millisecond: Long = timeCodeToMilliseconds(timecode, framesPerSecond) ?: throw IllegalArgumentException("'$timecode' is not a valid time code of the form HH:MM:SS:FF")
+
+    override fun niceText(): String = "TimeCodeTemporalPoint($timecode with $framesPerSecond fps)"
+
+    override fun toMilliseconds(): Long = millisecond
+
 }
--- backend/src/main/kotlin/dev/dres/data/serializers/TemporalPointSerializer.kt
+++ backend/src/main/kotlin/dev/dres/data/serializers/TemporalPointSerializer.kt
@@ -1,19 +1,40 @@
 package dev.dres.data.serializers
 
+import dev.dres.data.model.basics.time.FrameTemporalPoint
+import dev.dres.data.model.basics.time.MilliSecondTemporalPoint
 import dev.dres.data.model.basics.time.TemporalPoint
-import dev.dres.data.model.basics.time.TemporalUnit
+import dev.dres.data.model.basics.time.TimeCodeTemporalPoint
 import org.mapdb.DataInput2
 import org.mapdb.DataOutput2
 import org.mapdb.Serializer
 
 object TemporalPointSerializer: Serializer<TemporalPoint> {
     override fun serialize(out: DataOutput2, value: TemporalPoint) {
-        out.writeDouble(value.value)
-        out.writeInt(value.unit.ordinal)
+        when(value){
+            is FrameTemporalPoint -> {
+                out.packInt(1)
+                out.packInt(value.frame)
+                out.writeFloat(value.framesPerSecond)
+            }
+            is MilliSecondTemporalPoint -> {
+                out.packInt(2)
+                out.packLong(value.millisecond)
+            }
+            is TimeCodeTemporalPoint -> {
+                out.packInt(3)
+                out.writeUTF(value.timecode)
+                out.writeFloat(value.framesPerSecond)
+            }
+        }
+
     }
 
-    override fun deserialize(input: DataInput2, available: Int): TemporalPoint = TemporalPoint(
-            input.readDouble(),
-            TemporalUnit.values()[input.readInt()]
-    )
+    override fun deserialize(input: DataInput2, available: Int): TemporalPoint =
+        when(input.unpackInt()){
+            1 -> FrameTemporalPoint(input.unpackInt(), input.readFloat())
+            2 -> MilliSecondTemporalPoint(input.unpackLong())
+            3 -> TimeCodeTemporalPoint(input.readUTF(), input.readFloat())
+            else -> throw IllegalArgumentException("Unknown type of TemporalPoint")
+        }
+
 }

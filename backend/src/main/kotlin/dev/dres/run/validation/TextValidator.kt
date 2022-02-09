38e28c7f649ef46214c57e5970c2a5fbf690968a
Loris Sauter <loris.sauter@unibas.ch>
2022-02-09 11:46:32 +0100

    Slightly enhanced regex compatiblitiy for text targets

--- backend/src/main/kotlin/dev/dres/run/validation/TextValidator.kt
+++ backend/src/main/kotlin/dev/dres/run/validation/TextValidator.kt
@@ -4,10 +4,26 @@ import dev.dres.data.model.submissions.Submission
 import dev.dres.data.model.submissions.SubmissionStatus
 import dev.dres.data.model.submissions.aspects.TextAspect
 import dev.dres.run.validation.interfaces.SubmissionValidator
+import java.util.regex.Pattern
 
 class TextValidator(targets: List<String>) : SubmissionValidator {
 
-    private val regex = targets.map { it.toRegex() }
+    /**
+     * Transforms the targets to [Pattern]s.
+     * There is the convention introduced, that targets padded in backslashes (single) (\)
+     * are interpreted as regular expressions and the enclosing backslashes are removed.
+     * Regular Java pattern compilation rules apply.
+     * If the enclosing backslashes are missing, then the target is treated as a literal string.
+     *
+     * [Pattern.CANON_EQ] is activated for both, regex and literals.
+     */
+    private val patterns = targets.map {
+        if(it.startsWith("\\") && it.endsWith("\\")) {
+            Pattern.compile(it.substring(1, it.length - 1), Pattern.CANON_EQ)
+        } else {
+            Pattern.compile(it, Pattern.LITERAL or Pattern.CANON_EQ)
+        }
+    }
 
     override fun validate(submission: Submission) {
 
@@ -16,7 +32,7 @@ class TextValidator(targets: List<String>) : SubmissionValidator {
             return
         }
 
-        if (regex.any { it.matches(submission.text) }) {
+        if (patterns.any { it.matcher(submission.text).matches() })  {
             submission.status = SubmissionStatus.CORRECT
         } else {
             submission.status = SubmissionStatus.WRONG

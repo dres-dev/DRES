968159c366be376ecd5069a2924bcd5ea1e77ee5
Ralph Gasser <rg@pontius.ch>
2022-02-16 09:42:04 +0100

    Bugfix due to changes in data model.

--- frontend/src/app/utilities/time.utilities.ts
+++ frontend/src/app/utilities/time.utilities.ts
@@ -22,13 +22,11 @@ export class TimeUtilities {
     static point2Milliseconds(point: RestTemporalPoint, fps: number): number {
         switch (point.unit) {
             case 'FRAME_NUMBER':
-                return (parseFloat(point.value) / fps * 1000);
+                return (point.value / fps * 1000);
             case 'SECONDS':
-                return (parseFloat(point.value) * 1000);
+                return (point.value * 1000);
             case 'MILLISECONDS':
-                return parseFloat(point.value);
-            case 'TIMECODE':
-                return TimeUtilities.timeCode2Milliseconds(point.value, fps);
+                return point.value;
         }
     }
 

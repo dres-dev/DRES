9e0f730bcd89f1b9c1be2fcbd823a1671c76a38d
Loris Sauter <loris.sauter@unibas.ch>
2022-03-02 17:08:33 +0100

    Added some basic documentation on request

--- backend/src/main/kotlin/dev/dres/run/eventstream/StreamEvent.kt
+++ backend/src/main/kotlin/dev/dres/run/eventstream/StreamEvent.kt
@@ -11,14 +11,43 @@ import dev.dres.data.model.submissions.Submission
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "class")
 sealed class StreamEvent(var timeStamp : Long = System.currentTimeMillis(), var session: String? = null)
 
-
+/**
+ * Event indicating that the task was started.
+ */
 class TaskStartEvent(val runId: UID, val taskId: UID, val taskDescription: TaskDescription) : StreamEvent()
+
+/**
+ * Event indicating that the task was finished.
+ */
 class TaskEndEvent(val runId: UID, val taskId: UID) : StreamEvent()
+
+/**
+ * Event indicating that a run was started. The competition this run is an instance of is provided.
+ */
 class RunStartEvent(val runId: UID, val description: CompetitionDescription) : StreamEvent()
+
+/**
+ * Event indicating that a run was finished.
+ */
 class RunEndEvent(val runId: UID) : StreamEvent()
 
-
+/**
+ * Event indicating that a submission was received.
+ */
 class SubmissionEvent(session: String, val runId: UID, val taskId: UID?, val submission : Submission) : StreamEvent(session = session)
+
+/**
+ * Event indicating that a query event was logged (aka interaction log)
+ */
 class QueryEventLogEvent(session: String, val runId: UID, val queryEventLog: QueryEventLog) : StreamEvent(session = session)
+
+/**
+ * Event indicating that a query result was logged (aka result log)
+ */
 class QueryResultLogEvent(session: String, val runId: UID, val queryResultLog: QueryResultLog) : StreamEvent(session = session)
+
+/**
+ * Event indicating that a non-usable log was submitted (and thus was not properly stored).
+ * One reason for this event could be incomplete log data (e.g. JSON not properly closed)
+ */
 class InvalidRequestEvent(session: String, val runId: UID, val requestData: String) : StreamEvent(session = session)

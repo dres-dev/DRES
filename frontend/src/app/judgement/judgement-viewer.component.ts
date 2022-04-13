5067fc2560530c839527a372dcb0253c2b21f838
Loris Sauter <loris.sauter@unibas.ch>
2022-04-13 18:14:44 +0200

    Switched to yarn and eslint

--- frontend/src/app/judgement/judgement-viewer.component.ts
+++ frontend/src/app/judgement/judgement-viewer.component.ts
@@ -272,9 +272,9 @@ export class JudgementViewerComponent implements AfterViewInit, OnDestroy {
         let chr;
         for (i = 0; i < str.length; i++) {
             chr = str.charCodeAt(i);
-            // tslint:disable-next-line:no-bitwise
+            // eslint-disable-next-line no-bitwise
             hash = ((hash << 5) - hash) + chr;
-            // tslint:disable-next-line:no-bitwise
+            // eslint-disable-next-line no-bitwise
             hash |= 0; // Convert to 32bit integer
         }
         return hash as number;

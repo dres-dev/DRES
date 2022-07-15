7551208b185e9691f6c9f2345294038afc614213
Luca Rossetto <lucaro@lucaro.ch>
2022-07-15 19:17:51 +0200

    Added scripts to manage a tmux session

---
+++ doc/start_or_get_tmux_session.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+session="DRES"
+config="config.json"
+
+tmux has-session -t $session 2>/dev/null
+
+if [ $? != 0 ]; then
+  tmux new-session -d -s $session "./backend $config"
+fi
+
+tmux attach -t $session

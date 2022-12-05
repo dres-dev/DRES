5dfe85817eb5e824c8bfb95982514eeaf9ede825
Luca Rossetto <rossetto@ifi.uzh.ch>
2022-12-05 09:09:23 +0100

    Added missing limit to avoid negative scores

--- backend/src/main/kotlin/dev/dres/run/score/scorer/NewAvsTaskScorer.kt
+++ backend/src/main/kotlin/dev/dres/run/score/scorer/NewAvsTaskScorer.kt
@@ -1,6 +1,5 @@
 package dev.dres.run.score.scorer
 
-import dev.dres.data.model.UID
 import dev.dres.data.model.competition.TeamId
 import dev.dres.data.model.submissions.Submission
 import dev.dres.data.model.submissions.SubmissionStatus
@@ -9,6 +8,7 @@ import dev.dres.run.score.ScoreEntry
 import dev.dres.run.score.TaskContext
 import dev.dres.run.score.interfaces.RecalculatingSubmissionTaskScorer
 import dev.dres.run.score.interfaces.TeamTaskScorer
+import java.lang.Double.max
 import java.util.concurrent.locks.ReentrantReadWriteLock
 import kotlin.concurrent.read
 import kotlin.concurrent.write
@@ -16,7 +16,8 @@ import kotlin.concurrent.write
 /**
  * The new AVS Scorer.
  */
-class NewAvsTaskScorer(private val penaltyConstant: Double, private val maxPointsPerTask: Double) : RecalculatingSubmissionTaskScorer,
+class NewAvsTaskScorer(private val penaltyConstant: Double, private val maxPointsPerTask: Double) :
+    RecalculatingSubmissionTaskScorer,
     TeamTaskScorer {
 
     private var lastScores: Map<TeamId, Double> = emptyMap()
@@ -25,7 +26,8 @@ class NewAvsTaskScorer(private val penaltyConstant: Double, private val maxPoint
 
     constructor(parameters: Map<String, String>) : this(
         parameters.getOrDefault("penalty", "$defaultPenalty").toDoubleOrNull() ?: defaultPenalty,
-        parameters.getOrDefault("maxPointsPerTask", "$defaultMaxPointsPerTask").toDoubleOrNull() ?: defaultMaxPointsPerTask
+        parameters.getOrDefault("maxPointsPerTask", "$defaultMaxPointsPerTask").toDoubleOrNull()
+            ?: defaultMaxPointsPerTask
     )
 
     companion object {
@@ -38,13 +40,14 @@ class NewAvsTaskScorer(private val penaltyConstant: Double, private val maxPoint
         context: TaskContext
     ): Map<TeamId, Double> {
 
-        val distinctCorrectVideos = submissions.mapNotNullTo(mutableSetOf()) {//map directly to set and filter in one step
-            if (it !is ItemAspect || it.status != SubmissionStatus.CORRECT) {
-                null//filter all incorrect submissions
-            } else {
-                it.item.id
-            }
-        }.size
+        val distinctCorrectVideos =
+            submissions.mapNotNullTo(mutableSetOf()) {//map directly to set and filter in one step
+                if (it !is ItemAspect || it.status != SubmissionStatus.CORRECT) {
+                    null//filter all incorrect submissions
+                } else {
+                    it.item.id
+                }
+            }.size
 
         //no correct submissions yet
         if (distinctCorrectVideos == 0) {
@@ -54,17 +57,20 @@ class NewAvsTaskScorer(private val penaltyConstant: Double, private val maxPoint
         lastScores = this.lastScoresLock.write {
             submissions.filter { it is ItemAspect }.groupBy { it.teamId }.map { submissionsPerTeam ->
                 submissionsPerTeam.key to
-                    submissionsPerTeam.value.groupBy { submission ->
-                        submission as ItemAspect
-                        submission.item.id
-                    }.map {
-                        val firstCorrectIdx = it.value.sortedBy { s -> s.timestamp }.indexOfFirst { s -> s.status == SubmissionStatus.CORRECT }
-                        if (firstCorrectIdx < 0) { //no correct submissions, only penalty
-                            it.value.size * penaltyConstant
-                        } else { //apply penalty for everything before correct submission
-                            1.0 - firstCorrectIdx * penaltyConstant
-                        }
-                    }.sum() / distinctCorrectVideos * maxPointsPerTask //normalize
+                    max(0.0, //prevent negative total scores
+                        submissionsPerTeam.value.groupBy { submission ->
+                            submission as ItemAspect
+                            submission.item.id
+                        }.map {
+                            val firstCorrectIdx = it.value.sortedBy { s -> s.timestamp }
+                                .indexOfFirst { s -> s.status == SubmissionStatus.CORRECT }
+                            if (firstCorrectIdx < 0) { //no correct submissions, only penalty
+                                it.value.size * penaltyConstant
+                            } else { //apply penalty for everything before correct submission
+                                1.0 - firstCorrectIdx * penaltyConstant
+                            }
+                        }.sum() / distinctCorrectVideos * maxPointsPerTask //normalize
+                    )
             }.toMap()
         }
 

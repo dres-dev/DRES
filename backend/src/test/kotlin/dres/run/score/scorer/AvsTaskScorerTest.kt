4d252f1d93fcb50b8cc2ef1b2e842cc39aa3002e
Loris Sauter <loris.sauter@unibas.ch>
2022-12-05 14:04:46 +0100

    Legacy AVS Scoring unit tests: Fixed naming

--- backend/src/test/kotlin/dres/run/score/scorer/AvsTaskScorerTest.kt
+++ backend/src/test/kotlin/dres/run/score/scorer/AvsTaskScorerTest.kt
@@ -5,7 +5,7 @@ import dev.dres.data.model.basics.media.MediaItem
 import dev.dres.data.model.submissions.Submission
 import dev.dres.data.model.submissions.SubmissionStatus
 import dev.dres.run.score.TaskContext
-import dev.dres.run.score.scorer.LegacyAvsTaskScorer
+import dev.dres.run.score.scorer.AvsTaskScorer
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test
 
 class LegacyAvsTaskScorerTest {
 
-    private lateinit var scorer: LegacyAvsTaskScorer
+    private lateinit var scorer: AvsTaskScorer
     private val teams = listOf(UID(), UID(), UID()) //3 random team ids
     private val defaultTaskDuration = 5 * 60L
     private val dummyImageItems: List<MediaItem.ImageItem>
@@ -43,7 +43,7 @@ class LegacyAvsTaskScorerTest {
 
     @BeforeEach
     fun setup() {
-        this.scorer = LegacyAvsTaskScorer()
+        this.scorer = AvsTaskScorer()
     }
 
     @Test

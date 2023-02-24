b888ba0401fc0f4d41d03d854fa8a27c3683c8ab
Ralph Gasser <rg@pontius.ch>
2023-02-24 15:50:18 +0100

    ScoresUpdatable now proactively informs clients about updates via WebSocket.

--- backend/src/main/kotlin/dev/dres/run/updatables/ScoresUpdatable.kt
+++ backend/src/main/kotlin/dev/dres/run/updatables/ScoresUpdatable.kt
@@ -1,15 +1,21 @@
 package dev.dres.run.updatables
 
+import dev.dres.api.rest.types.evaluation.websocket.ServerMessage
+import dev.dres.api.rest.types.evaluation.websocket.ServerMessageType
 import dev.dres.data.model.run.AbstractInteractiveTask
 import dev.dres.data.model.run.interfaces.TaskId
 import dev.dres.data.model.submissions.DbSubmission
 import dev.dres.data.model.submissions.DbAnswerSet
+import dev.dres.run.RunExecutor
 import dev.dres.run.RunManager
 import dev.dres.run.RunManagerStatus
+import dev.dres.run.score.ScoreTimePoint
 import dev.dres.run.score.Scoreable
+import dev.dres.run.score.scoreboard.Scoreboard
 import kotlinx.dnq.query.asSequence
 import kotlinx.dnq.query.filter
 import kotlinx.dnq.query.mapDistinct
+import java.util.*
 import kotlin.collections.LinkedHashSet
 
 /**
@@ -43,6 +49,7 @@ class ScoresUpdatable(private val manager: RunManager): Updatable {
         val removed = mutableListOf<TaskId>()
         if (this.set.isNotEmpty()) {
             this.manager.store.transactional(true) {
+                /* Update scores. */
                 this.set.removeIf { task ->
                     val submissions = DbAnswerSet.filter { a -> a.task.id eq task.taskId }.mapDistinct { it.submission }.asSequence()
                     val scores = task.scorer.scoreMap(submissions)
@@ -51,6 +58,9 @@ class ScoresUpdatable(private val manager: RunManager): Updatable {
                     true
                 }
             }
+
+            /* Inform clients about changes. */
+            removed.forEach { RunExecutor.broadcastWsMessage(ServerMessage(this.manager.evaluation.id, ServerMessageType.TASK_UPDATED, it)) }
         }
     }
 

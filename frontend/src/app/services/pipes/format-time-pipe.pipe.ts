7cb94e5dd5340a6bf7d3a4254bd89dbaedc0cb64
Loris Sauter <loris.sauter@unibas.ch>
2023-03-24 16:01:25 +0100

    Addressing #367 by using milliseconds

--- frontend/src/app/services/pipes/format-time-pipe.pipe.ts
+++ frontend/src/app/services/pipes/format-time-pipe.pipe.ts
@@ -8,18 +8,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatTimePipePipe implements PipeTransform {
   transform(value: number): string {
-    const hrs = Math.floor(value / 3600);
-    const mins = Math.floor((value % 3600) / 60);
-    const secs = Math.floor(value % 60);
+    const hrs = Math.floor((value/1000) / 3600);
+    const mins = Math.floor(((value/1000) % 3600) / 60);
+    const secs = Math.floor((value/1000) % 60);
+    const ms = Math.floor(value % 1000);
     let out = '';
     /* Hours if present */
     if (hrs > 0) {
-      out += '' + (hrs < 10 ? '0' : '') + hrs + ':';
+      out += '' + (''+hrs).padStart(2, '0') + ':';
     }
     /* Minutes */
-    out += '' + (mins < 10 ? '0' : '') + mins + ':';
-    /* seconds */
-    out += '' + (secs < 10 ? '0' : '') + secs;
+    out += '' + (''+mins).padStart(2, '0') + ':';
+    /* Seconds */
+    out += '' + (''+secs).padStart(2, '0') + '.';
+    /* Milliseconds */
+    out += '' + (''+ms).padStart(3,'0');
     return out;
   }
 }

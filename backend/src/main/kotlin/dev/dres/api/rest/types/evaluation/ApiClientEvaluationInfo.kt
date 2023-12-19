c8e0422e9da94928d4873e4e8c6a678c08ff02e4
Luca Rossetto <lucaro@lucaro.ch>
2023-12-19 21:17:53 +0100

    Changed return type of ClientListEvaluationsHandler

---
+++ backend/src/main/kotlin/dev/dres/api/rest/types/evaluation/ApiClientEvaluationInfo.kt
@@ -0,0 +1,38 @@
+package dev.dres.api.rest.types.evaluation
+
+import dev.dres.data.model.run.ApiRunProperties
+import dev.dres.run.InteractiveAsynchronousRunManager
+import dev.dres.run.InteractiveSynchronousRunManager
+import dev.dres.run.NonInteractiveRunManager
+import dev.dres.run.RunManager
+
+/**
+ * Contains the basic and most importantly static information about a [RunManager] that is relevant to a participant.
+ *
+ */
+data class ApiClientEvaluationInfo(
+    val id: String,
+    val name: String,
+    val type: ApiEvaluationType,
+    val status: ApiEvaluationStatus,
+    val templateId: String,
+    val templateDescription: String?,
+    val teams: List<String>,
+    val taskTemplates: List<ApiClientTaskTemplateInfo>,
+) {
+    constructor(manager: RunManager): this(
+        manager.id,
+        manager.name,
+        when(manager) {
+            is InteractiveSynchronousRunManager -> ApiEvaluationType.SYNCHRONOUS
+            is InteractiveAsynchronousRunManager -> ApiEvaluationType.ASYNCHRONOUS
+            is NonInteractiveRunManager -> ApiEvaluationType.NON_INTERACTIVE
+            else -> throw IllegalStateException("Incompatible type of run manager.")
+        },
+        manager.status.toApi(),
+        manager.template.id,
+        manager.template.description,
+        manager.template.teams.asSequence().map { team -> team.name ?: "" }.toList(),
+        manager.template.tasks.map { task -> ApiClientTaskTemplateInfo(task) }.toList()
+    )
+}
---
+++ backend/src/main/kotlin/dev/dres/api/rest/types/evaluation/ApiClientTaskTemplateInfo.kt
@@ -0,0 +1,21 @@
+package dev.dres.api.rest.types.evaluation
+
+import dev.dres.api.rest.types.template.tasks.ApiTaskTemplate
+import dev.dres.data.model.template.task.DbTaskTemplate
+
+/**
+ * Basic and most importantly static information about a [DbTaskTemplate] relevant to the participant.
+ */
+data class ApiClientTaskTemplateInfo(
+    val name: String,
+    val taskGroup: String,
+    val taskType: String,
+    val duration: Long
+) {
+    constructor(task: ApiTaskTemplate) : this(
+        task.name,
+        task.taskGroup,
+        task.taskType,
+        task.duration
+    )
+}

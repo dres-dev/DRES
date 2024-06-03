4b79f3835023f6a1e0c58ff6156cc5f38f84232f
Loris Sauter <loris.sauter@unibas.ch>
2024-06-03 20:30:27 +0200

    Prepare next release cycle

--- backend/src/main/kotlin/dev/dres/DRES.kt
+++ backend/src/main/kotlin/dev/dres/DRES.kt
@@ -42,7 +42,7 @@ import kotlin.system.exitProcess
  */
 object DRES {
     /** Version of DRES. */
-    const val VERSION = "2.0.4"
+    const val VERSION = "2.0.5-SNAPSHOT"
 
     /** Application root; should be relative to JAR file or classes path. */
     val APPLICATION_ROOT: Path =
